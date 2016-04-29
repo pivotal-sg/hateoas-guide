6055267f4b6f0746fb22cb7c9fe29819bb1be759
Yifeng Hou <yhou@pivotal.io>
2016-04-29 16:04:09 +0800

    Initial commit
    Signed-off-by: Björn Andersson <bandersson@pivotal.io>

---
+++ src/main/java/sg/pivotal/HateoasGuideApplication.java
@@ -0,0 +1,11 @@
+package sg.pivotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class HateoasGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasGuideApplication.class, args);
+	}
+}
